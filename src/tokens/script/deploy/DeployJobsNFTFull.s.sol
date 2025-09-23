a16a6da3520d0c609d34f7d1021511ed728e7ec5
Lovro Posel <lovro.posel79@gmail.com>
2025-09-23 22:24:00 +0200

    Add files via upload

---
+++ src/tokens/script/deploy/DeployJobsNFTFull.s.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+import {JobsNFTFull} from "src/JobsNFTFull.sol";
+
+contract JobsNFTFullScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        address initialOwner = 0x712893c6660C2AFceFC63D0F3A4e7269EE3637ee;
+
+        vm.startBroadcast();
+        JobsNFTFull instance = new JobsNFTFull(initialOwner);
+        console.log(" Contract deployed at: %s", address(instance));
+        vm.stopBroadcast();
+    }
+}
